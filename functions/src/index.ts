16d0b4d2a05b9a9bde87e267862da2824b02073d
Rohit <tprohit9@gmail.com>
2020-05-10 06:22:59 +0000

    Added .gitpod.yml

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,33 @@
 import * as functions from 'firebase-functions';
+import fetch from 'node-fetch';
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-const VIDEO_ID = "&id=_ylFqO-QZL0"
-const API_KEY = "&key="
-const API_URL = "https://www.googleapis.com/youtube/v3/videos?part=statistics"
+const VIDEO_ID = "sOfC0rcrCs8"
+const API_KEY = "&key=" + functions.config().youtube.api_key
+const GET_URL = "https://www.googleapis.com/youtube/v3/videos?part=statistics&id="
+const INSERT_URL = "https://www.googleapis.com/youtube/v3/videos?part=snippet"
 
-export const updateViews = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+export const updateViews = functions.https.onRequest((_request, response) => {
+    fetch(GET_URL+VIDEO_ID+API_KEY).then(res => res.json())
+        .then((json) => json.items[0].statistics.viewCount)
+        .then((views) => {
+            console.log(views);
+            
+            let param = { 
+                method: 'PUT', 
+                body: JSON.stringify({
+                    id: VIDEO_ID,
+                    snippet: {
+                        //description: "This description is in English.",
+                        title: "This video has "+views+" views"
+                    }
+                }),
+                headers: { 'Content-Type': 'application/json' }
+            };
+            return fetch(INSERT_URL + API_KEY, param);
+        }).then(res => res.json())
+        .then(response.json)
+        .catch(response.send)
 });
